d33eb515b3105b989b1b2ffb346f11e36dcc513c
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2025-07-28 18:16:28 +0700

    Create layout admin

---
+++ AirlineBooking/AirlineWeb/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AirlineWeb.Common;
+using AirlineWeb.Models;
+using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AirlineWeb.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataAirline db = new DataAirline();
+
+        #region Home
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                // Log lỗi
+                await Logger.ErrorAsync(ex);
+
+                // Trả về trang 404
+                return RedirectToAction("NotFound", "Notification", new { area = "Customer" });
+            }
+        }
+        #endregion
+
+        #region Giải phóng tài nguyên
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #endregion
+    }
+}
